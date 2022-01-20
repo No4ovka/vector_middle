b9eb89e399d2ee7d94aa45868969c3450d4fd3a6
No4ovka <92082996+No4ovka@users.noreply.github.com>
2022-01-20 19:28:06 +0300

    Add files via upload

--- join.cpp
+++ join.cpp
@@ -5,8 +5,9 @@ string itc_join(vector <char> lst, string sep)
     string str = "";
     if (lst.size() > 0)
     {
-        for (int k = 0; k < lst.size(); k++)
+        for (int k = 0; k < lst.size() - 1; k++)
             str += lst[k] + sep;
     }
+    str += lst[lst.size() - 1];
     return str;
 }
